907566b620bb9d7a02e43e82d7fa5ef5d1b089a6
QuocDat132 <90312750+QuocDat132@users.noreply.github.com>
2021-10-21 14:45:06 +0700

    Add files via upload

---
+++ phongDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+   public class phongDTO
+    {
+        public string maPhong { get; set; }
+        public string tinhTrang { get; set; }
+        public string loaiPhong { get; set; }
+        public float donGia { get; set; }
+    }
+}
