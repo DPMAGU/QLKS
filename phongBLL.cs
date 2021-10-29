5c35f12c7ca936944cdbeb10ea8277005f9b937a
QuocDat132 <90312750+QuocDat132@users.noreply.github.com>
2021-10-29 08:21:37 +0700

    Add files via upload

---
+++ phongBLL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using DAL;
+using DTO;
+
+namespace BLL
+{
+   public class phongBLL
+    {
+        phongDAL Stclr = new phongDAL();
+        public List<phongDTO> hienthithongtinphong()
+        {
+            var thongTin = Stclr.hienthithongtinphong();
+            var danhSachPhong = new List<phongDTO>();
+            foreach (var item in thongTin.Rows)
+            {
+                var p = (DataRow)item;
+                var phong = new phongDTO()
+                {
+                    maPhong = $"{p["maPhong"]}",
+                    tinhTrang = $"{p["tinhTrang"]}",
+                    loaiPhong = $"{p["loaiPhong"]}",
+                    donGia = float.Parse($"{p["donGia"]}")
+
+                };
+                danhSachPhong.Add(phong);
+            }
+            return danhSachPhong;
+        }
+    }
+}
