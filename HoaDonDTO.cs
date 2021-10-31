21b7df3951f8da4b66f2df69405d9421def5e350
QuocDat132 <90312750+QuocDat132@users.noreply.github.com>
2021-10-31 16:53:44 +0700

    Add files via upload

---
+++ HoaDonDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class HoaDonDTO
+    {
+        public int maHoaDon { get; set; }
+        public int maKhachHang { get; set; }
+        public int? maNhanVien { get; set; }
+        public int tongTien { get; set; }
+        public DateTime ngayLap { get; set; }
+    }
+}
