a0f49f1a4d8bf64698f7fef32eb4cbd346d5cd3e
QuocDat132 <90312750+QuocDat132@users.noreply.github.com>
2021-11-11 22:27:40 +0700

    Add files via upload

---
+++ HoaDonBLL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DAL;
+using DTO;
+
+namespace BLL
+{
+    public class HoaDonBLL
+    {
+        public void DanhSachHoaDon(DataGridView dt)
+        {
+            var danhSach = new HoaDonDAL();
+            dt.DataSource = null;
+            dt.DataSource = danhSach.DanhSachHoaDon();
+        }
+
+        public void LapHoaDon(HoaDonDTO hd)
+        {
+            var lap = new HoaDonDAL();
+            lap.LapHoaDon(hd);
+        }
+
+    }
+
+   
+}
