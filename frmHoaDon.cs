7f13a3b0420de6525f6790d85a3d6022e090e4c9
QuocDat132 <90312750+QuocDat132@users.noreply.github.com>
2021-11-11 22:28:38 +0700

    Add files via upload

---
+++ frmHoaDon.cs
@@ -0,0 +1,29 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quanlykhachsan
+{
+    public partial class frmHoaDon : Form
+    {
+        public frmHoaDon()
+        {
+            InitializeComponent();
+            LoadDanhSach();
+        }
+
+        private void LoadDanhSach()
+        {
+            var danhSach = new HoaDonBLL();
+            danhSach.DanhSachHoaDon(dataGridViewHoaDon);     
+        }
+        
+    }
+}
