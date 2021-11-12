76d75b7e386f35cd48e68b8d2c7c7a400b84dde1
Danie-Tr <88081534+Danie-Tr@users.noreply.github.com>
2021-11-12 20:29:28 +0700

    Add files via upload

---
+++ nguoidungBLL.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using DAL;
+using DTO;
+
+
+namespace BLL
+{
+    public class nguoidungBLL
+    {
+        NguoidungDAL ndDAL = new NguoidungDAL();
+        public List<NguoiDungDTO> Hienthinguoidung()
+        {
+            var listNguoiDung = new List<NguoiDungDTO>();
+            var dtNguoidung = ndDAL.hienthinguoidung();
+            for (int i = 0; i < dtNguoidung.Rows.Count; i++)
+            {
+                listNguoiDung.Add(new NguoiDungDTO()
+                {
+                    TaiKhoan = dtNguoidung.Rows[i]["taiKhoan"].ToString(),
+                    MatKhau = dtNguoidung.Rows[i]["matKhau"].ToString(),
+                    PhanQuyen = dtNguoidung.Rows[i]["phanQuyen"].ToString(),
+                    maNhanVien = int.Parse(dtNguoidung.Rows[i]["maNhanVien"].ToString()),
+                });
+            }
+            return listNguoiDung;
+            
+        }
+
+        public bool them_nguoidung( string taiKhoan, string matKhau, string phanQuyen, int maNhanVien)
+        {
+            return ndDAL.them_nguoidung(taiKhoan, matKhau, phanQuyen, maNhanVien);
+        }
+
+         public void xoa_nguoidung(string taiKhoan)
+        {
+            ndDAL.xoa_nguoidung(taiKhoan);
+        }
+
+        
+
+        public void sua_nguoidung(string taiKhoan, string matKhau, string phanQuyen, int maNhanVien)
+        {
+            ndDAL.sua_nguoidung(taiKhoan, matKhau, phanQuyen, maNhanVien);
+        }
+    }
+}
---
+++ nguoidungDAL.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class NguoidungDAL
+    {
+        public DataTable hienthinguoidung()
+        {
+            SqlConnection cnn = ketnoi.MoKetNoi();
+            // Khai báo và khởi tạo đối tượng Command, truyền vào tên thủ tục tương ứng
+            DataTable dt = ketnoi.TruyVanLayDuLieu("select * from nguoidung", cnn);
+            // Khai báo kiểu thực thi là Thực thi thủ tục
+            ketnoi.DongKetNoi(cnn);   
+            cnn.Close();
+            return dt;
+        }
+        public bool them_nguoidung(string taiKhoan,  string matKhau, string phanQuyen, int maNhanVien)
+        {
+            SqlConnection cnn = ketnoi.MoKetNoi();
+            SqlCommand cmd = new SqlCommand("them_nguoidung", cnn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("taiKhoan", taiKhoan);
+            cmd.Parameters.AddWithValue("matKhau", matKhau);
+            cmd.Parameters.AddWithValue("phanQuyen", phanQuyen);
+            cmd.Parameters.AddWithValue("maNhanVien", maNhanVien);
+
+            int i = cmd.ExecuteNonQuery();
+            cnn.Close();
+            if (i != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void xoa_nguoidung(string taiKhoan)
+        {
+
+            SqlConnection cnn = ketnoi.MoKetNoi();
+            SqlCommand cmd = new SqlCommand("xoa_nguoidung", cnn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("taiKhoan", taiKhoan);
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+
+        }
+
+        public void sua_nguoidung(string taiKhoan, string matKhau, string phanQuyen, int maNhanVien)
+        {
+            SqlConnection cnn = ketnoi.MoKetNoi();
+            SqlCommand cmd = new SqlCommand("sua_nguoidung", cnn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("taiKhoan", taiKhoan);
+            cmd.Parameters.AddWithValue("matKhau", matKhau);
+            cmd.Parameters.AddWithValue("phanQuyen", phanQuyen);
+            cmd.Parameters.AddWithValue("maNhanVien", maNhanVien);
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+    }
+}
