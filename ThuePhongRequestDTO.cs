df8a7973ddcfc8a672163f2414e02cbebe71c911
Thaoho240100 <90312455+Thaoho240100@users.noreply.github.com>
2021-11-12 09:02:10 +0700

    Add files via upload

---
+++ ThuePhongRequestDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class ThuePhongRequestDTO
+    {
+        // maThuePhong, a.maKhachHang, a.maPhong, tenKhachHang,ngaySinh,chungMinhNhanDan,diaChi,soDienThoai,ngayDen,donGia
+        public int maThuePhong { get; set; }
+        public int maKhachHang { get; set; }
+        public int maPhong { get; set; }
+        public string tenKhachHang { get; set; }
+        public DateTime ngaySinh { get; set; }
+        public string chungMinhNhanDan { get; set; }
+        public string diaChi { get; set; }
+        public string soDienThoai { get; set; }
+        public DateTime ngayDen { get; set; }
+        public float donGia { get; set; }
+    }
+}
